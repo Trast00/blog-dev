417dffaa5366c209703b93ca6aced8ebca88a962
Allassane <dickoallassane1997@gmail.com>
2023-04-06 13:39:51 +0100

    Add test for Comments

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,30 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe Like, type: :model do
+RSpec.describe Comment, type: :model do
   subject do
-    Like.create(
+    Comment.create(
       post: Post.create(author: User.create(name: 'Tom',
                                             photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
                                             bio: 'Teacher from Mexico.'), title: 'Hello2',
                         text: 'This is my first post'),
       author: User.create(name: 'Tom',
                           photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                          bio: 'Teacher from Mexico.')
+                          bio: 'Teacher from Mexico.'),
+      text: 'Hi eveyone'
     )
   end
 
   before { subject.save }
 
-  it 'Like should be valid' do
+  it 'Comment should be valid' do
     expect(subject).to be_valid
   end
 
-  it 'Like should not be valid withou author' do
+  it 'Comment should not be valid withou author' do
     subject.author = nil
     expect(subject).to_not be_valid
   end
 
-  it 'Like should not be valid withou post' do
+  it 'Comment should not be valid withou post' do
     subject.post = nil
     expect(subject).to_not be_valid
   end
