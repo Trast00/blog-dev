0e25a23679efd8684269962934bdbfe9a95d97df
Allassane <dickoallassane1997@gmail.com>
2023-04-10 15:15:42 +0100

    Add users post page and route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/", to: "users#index"
+  get "/", to: "users#index" 
   get "/users/:id", to: "users#show"
+  
+  get "/users/:id/posts", to: "posts#index"
 end
