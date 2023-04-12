f577ad2fdd26a457a2d0b3cf701ab7850db0f4cd
Allassane <dickoallassane1997@gmail.com>
2023-04-12 16:21:15 +0100

    Setup default time zone

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module BlogDev
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-
+    
+    config.time_zone = 'Eastern Time (US & Canada)'
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
