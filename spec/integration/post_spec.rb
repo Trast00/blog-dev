18000f170801df38743dc2928ecd9e9301875a3f
PrantoshB <pbiswas1007@gmail.com>
2023-04-18 18:05:02 +0530

    Create test for post show

--- spec/integration/post_spec.rb
+++ spec/integration/post_spec.rb
@@ -76,5 +76,44 @@ RSpec.describe "Post", type: :system do
 
   end
 
+  context "test post show page" do
+    before(:all) do
+      @post = Post.includes(:comments, :author).find(@posts[0].id)
+      visit "/users/#{@user.id}/posts/#{@post.id}"
+    end
 
+    it 'can see who wrote the post' do
+      visit "/users/#{@user.id}/posts/#{@post.id}"
+      expect(page).to have_text("by #{@user.name}")
+    end
+
+    it "can see how many comments a post has" do
+      visit "/users/#{@user.id}/posts/#{@post.id}"
+      expect(page).to have_text("Comments:")
+    end
+
+    it "can see how many likes a post has" do
+      visit "/users/#{@user.id}/posts/#{@post.id}"
+      expect(page).to have_text("Likes:")
+    end
+
+    it "can see the post body" do
+      visit "/users/#{@user.id}/posts/#{@post.id}"
+      expect(page).to have_text(@post.text)
+    end
+
+    it "can see the username of each commentor" do
+      @post.comments.each do |comment|
+        visit "/users/#{@user.id}/posts/#{@post.id}"
+        expect(page).to have_text(comment.author.name)
+      end
+    end
+
+    it "can see the comment each commentor left" do
+      @post.comments.each do |comment|
+        visit "/users/#{@user.id}/posts/#{@post.id}"
+        expect(page).to have_text(comment.text)
+      end
+    end
+  end
 end
